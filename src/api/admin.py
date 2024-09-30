b3dac9721ffc0a00e21af7328f4a673d53faefeb
s6hib <sahibkullar@gmail.com>
2024-09-30 14:10:03 -0700

    Add reset functionality

--- src/api/admin.py
+++ src/api/admin.py
@@ -18,10 +18,5 @@ def reset():
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
     with db.engine.begin() as connection:
-        connection.execute(
-            sqlalchemy.text(
-                "UPDATE global_inventory \
-                    SET num_green_potions = 0, num_green_ml = 0, gold = 100"
-            )
-        )
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = 0, num_green_ml = 0, gold = 100"))
     return "OK"
